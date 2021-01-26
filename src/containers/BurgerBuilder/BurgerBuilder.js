e9667e87ec735b98a75a067a38c02a3336cb9465
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-01-26 16:27:53 +0530

    Price section

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,7 +21,20 @@ class BurgerBuilder extends Component{
             cheese: 0,
             meat:0
         },
-        totalPrice : 2
+        totalPrice : 2,
+        purchasable : false
+    }
+
+    updatePurchaseState(ingredients){
+        
+        const sum = Object.keys(ingredients)
+                    .map(igKey => {
+                        return ingredients[igKey]
+                    })
+                    .reduce((sum, el)=>{
+                        return sum + el;
+                    },0);
+        this.setState({purchasable : sum > 0});
     }
 
     addIngredientHandler =(type) =>{
@@ -39,6 +52,7 @@ class BurgerBuilder extends Component{
             totalPrice : newPrice,
             ingredients : updatedIngredients
         })
+        this.updatePurchaseState(updatedIngredients);
     }
 
     removeIngredientHandler = (type) =>{
@@ -58,7 +72,8 @@ class BurgerBuilder extends Component{
         this.setState({
             totalPrice : newPrice,
             ingredients : updatedIngredients
-        })
+        });
+        this.updatePurchaseState(updatedIngredients);
     }
 
     render(){
@@ -79,6 +94,7 @@ class BurgerBuilder extends Component{
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled = {disabledInfo}
                     price = {this.state.totalPrice}
+                    purchasable={this.state.purchasable}
                      />
 
                 </div>
